16cf8eb76adb6966e13ebcb8e5dacd86e52dbd4f
Suphachok <suphachok.h@ku.th>
2022-10-11 21:38:58 +0700

    update crop,work

--- src/main/java/models/Crop.java
+++ src/main/java/models/Crop.java
@@ -1,8 +1,12 @@
 package models;
 
 public class Crop {
-    private String crop_id;
+    public String crop_id;
     private double quantity_Seed;
     private double farm_size;
     private String date_crop;
+    private String date_harvest;
+    private double h_quantity;
+    private int income;
+    private int harvested_times;
 }
--- src/main/java/models/Work.java
+++ src/main/java/models/Work.java
@@ -1,5 +1,8 @@
 package models;
 
 public class Work {
+    public String n_date;
     private String n_type;
+    private int n_quantity;
+    private String statusNourishment;
 }
