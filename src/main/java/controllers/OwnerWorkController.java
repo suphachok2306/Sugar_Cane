30e0db5cf67903556afc75170a0d51c8536304b8
Suphachok <suphachok.h@ku.th>
2022-10-27 18:29:34 +0700

    Work,OwnerWorkController

--- src/main/java/controllers/OwnerWorkController.java
+++ src/main/java/controllers/OwnerWorkController.java
@@ -1,4 +1,82 @@
 package controllers;
+import com.github.saacsos.FXRouter;
+import javafx.scene.control.Alert;
+import models.Work;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class OwnerWorkController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OwnerWorkController implements Initializable {
+    private Work selectedWork;
+
+    @FXML
+    private TableView<Work> tableView;
+
+    @FXML
+    public TableColumn<Work, String> workName;
+
+    @FXML
+    public TableColumn<Work, String> statusWork;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        workName.setCellValueFactory(new PropertyValueFactory<>("WorkName"));
+        statusWork.setCellValueFactory(new PropertyValueFactory<>("StatusWork"));
+        //add your data to the table here.
+        tableView.setItems(works);
+
+        tableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                selectedWork = newValue;
+            }
+        });
+    }
+
+    private ObservableList<Work> works = FXCollections.observableArrayList(
+            new Work("Crop","Not assigned"),
+            new Work("Restoration","Not assigned"),
+            new Work("Caring","Not assigned"),
+            new Work("Harvest","Not assigned")
+    );
+
+    @FXML
+    private void confirmWork() {
+        if (selectedWork == null){
+            Alert error = new Alert(Alert.AlertType.ERROR, " Please select work.");
+            error.show();
+        }
+    }
+
+    @FXML
+    private void switchToCheckWork() {
+        try {
+            if (selectedWork == null){
+                Alert error = new Alert(Alert.AlertType.ERROR, " Please select work.");
+                error.show();
+            }
+            else {
+                FXRouter.goTo("checkWork", selectedWork);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void logoutButton() {
+        try {
+            FXRouter.goTo("login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/models/Work.java
+++ src/main/java/models/Work.java
@@ -1,8 +1,31 @@
 package models;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Work {
-    public String n_date;
-    private String n_type;
-    private int n_quantity;
-    private String statusNourishment;
+
+    private SimpleStringProperty workName;
+    private SimpleStringProperty statusWork;
+
+    public Work(String workName,String statusWork) {
+        this.workName = new SimpleStringProperty(workName);
+        this.statusWork = new SimpleStringProperty(statusWork);
+    }
+
+
+    public String getWorkName() {
+        return workName.get();
+    }
+
+    public void setWorkName(String workName) {
+        this.workName = new SimpleStringProperty(workName);
+    }
+
+    public String getStatusWork() {
+        return statusWork.get();
+    }
+
+    public void setStatusWork(String statusWork) {
+        this.statusWork = new SimpleStringProperty(statusWork);
+    }
 }
